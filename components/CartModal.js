61ade4b427694a9528f5779157654de46e4a9b6e
liferacer23 <binyamg23@gmail.com>
2022-04-25 19:46:10 +0300

    cart modal done

--- components/CartModal.js
+++ components/CartModal.js
@@ -13,14 +13,14 @@ export default function CartModal({ setCartFlipper }) {
         }}
         className="fixed inset-0 bg-gray-800 bg-opacity-50 overflow-y-auto h-full w-full"
       ></div>
-      <div className=" absolute bg-white top-0 right-0 h-full z-50 w-3/5 flex flex-col p-2 items-center px-14">
+      <div className=" overflow-y-auto overflow-x-hidden absolute bg-white top-0 right-0 h-full z-50 w-3/5 flex flex-col p-2 items-center px-14">
         <div className="flex w-full h-16 justify-end items-center p-2">
-          <div className="relative cursor-pointer flex items-center justify-between w-24 h-8 border-2 border-gray-200 rounded-2xl px-4">
-            <button
-              onClick={() => {
+          <div  onClick={() => {
                 setCartFlipper((prev) => !prev);
-              }}
-              className="text-xs text-black font-bold cursor-pointer text-white rounded-2xl h-8 w-1/6 "
+              }} className="relative cursor-pointer flex items-center justify-between w-24 h-8 border-2 border-gray-200 rounded-2xl px-4">
+            <button
+             
+              className="text-xs text-black font-bold cursor-pointer rounded-2xl h-8 w-1/6 "
             >
               Close
             </button>
@@ -147,6 +147,21 @@ export default function CartModal({ setCartFlipper }) {
               </div>
             </div>
             
+          </div>
+          <hr className="mt-2"/>
+        </div>
+        <div className="mt-40 flex w-full flex-col space-y-2 p-2">
+        <hr className="mt-1"/>
+          <div className="flex items-center justify-between">
+            <h1 className="text-xs font-bold">TOTAL INC TAX</h1>
+            <h1 className="text-xs font-bold">$1,245</h1>
+          </div>
+          <hr className="mt-1"/>
+          <div className=" flex w-full justify-end mt-2">
+              <div className="flex w-4/6 space-x-3">
+                <button className="border-2 border-gray-500 rounded-full text-black-500 text-xs h-9 w-48">CONTINUE SHOPPING</button>
+                <button className="border-2 border-black-500 rounded-full text-white text-xs bg-black  h-9 w-48">PROCESS TO CHECKOUT</button>
+              </div>
           </div>
         </div>
       </div>
