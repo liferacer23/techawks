2506e324f776cb06fd4eba7b5eeb16f188bdb4c4
liferacer23 <binyamg23@gmail.com>
2022-05-02 21:17:49 +0300

    font-bug

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
           ensures its consistently high ranking among shopping sites.
         </p>
       </div>
-{/*       <div
+    {/*   <div
       className="max-w-screen-xl mt-0 mb-6 sm:mt-14 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto"
       id="feature"
     >
@@ -84,9 +84,9 @@ export default function Home() {
               Sports and Travel
             </li>
           </ul>
-        </div> */}
+        </div>
       </div>
-    </div>
+    </div> */}
     </div>
   );
 }
